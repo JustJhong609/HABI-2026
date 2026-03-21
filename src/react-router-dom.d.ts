43da0f720ce0dda2178ff481a93dc471ecc344a1
Jhong Emata <20211199@nbsc.edu.ph>
2026-03-21 07:02:22 +0000

    vite ready

--- src/react-router-dom.d.ts
+++ src/react-router-dom.d.ts
@@ -1,6 +1,20 @@
 declare module "react-router-dom" {
   import * as React from "react";
 
+  export interface BrowserRouterProps {
+    children?: React.ReactNode;
+  }
+
+  export class BrowserRouter extends React.Component<BrowserRouterProps> {}
+
+  export interface RouteProps {
+    path?: string;
+    exact?: boolean;
+    component?: React.ComponentType<any>;
+    render?: (props: any) => React.ReactNode;
+    children?: React.ReactNode;
+  }
+
   export type HistoryLike = {
     push: (path: string) => void;
     replace: (path: string) => void;
@@ -8,6 +22,6 @@ declare module "react-router-dom" {
 
   export function useHistory(): HistoryLike;
 
-  export const Redirect: React.ComponentType<any>;
-  export const Route: React.ComponentType<any>;
+  export const Redirect: React.ComponentType<{ to: string }>;
+  export const Route: React.ComponentType<RouteProps>;
 }
